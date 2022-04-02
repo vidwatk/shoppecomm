2f6e8368d5a5b13d358cedc334ef7e5d216192f0
Vidwat Kanade <vidwatk@gmail.com>
2022-04-03 00:24:21 +0100

    CategoryFilter(...js added): added the categories (incomplete)

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -5,27 +5,41 @@ import { Icon, Container, Header, Item, Text, Input } from "native-base"
 import ProductList from "./ProductList"
 import SearchedProducts from './SearchedProducts';
 import Banner from '../../Shared/Banner';
+import CategoryFilter from './CategoryFilter';
 
 const data = require("../../assets/products.json");
+const productCategories = require("../../assets/categories.json")
+//console.log(categories);
 //console.log(data);
 const ProductContainer = () => {
 
     const[products, setProducts] = useState([]);
     const[productsFiltered, setproductsFiltered] = useState([]);
     const[focus, setFocus] = useState();
+    const[categories, setCategories] = useState([]);
+    const[productsCtg, setProductsCtg] = useState([]);
+    const[active, setActive] = useState();
+    const[initialState, stateInitialState] = useState();
 
     useEffect(() => {
         setProducts(data);
         setproductsFiltered(data);
         setFocus(false);
+        setCategories(productCategories);
+        setActive(-1);
+        stateInitialState(data);
 
         return() =>{
             setProducts([])
             setproductsFiltered([])
             setFocus()
+            setCategories([])
+            setActive()
+            stateInitialState()
         }
     }, [])
 
+    //Product Methods
     const searchProduct = (text) => {
         setproductsFiltered(
             products.filter((i) => i.name.toLowerCase().includes(text.toString().toLowerCase()))
@@ -38,6 +52,20 @@ const ProductContainer = () => {
 
     const onBlur = () => {
         setFocus(false);
+    }
+
+    //Categories
+    const changeCtg = (ctg) => {
+        {
+            ctg === "all"
+                ?[setProductsCtg(initialState), setActive(true)]
+                : [
+                    setProductsCtg(
+                        products.filter((i) => i.category._id === ctg),
+                        setActive(true)
+                    )
+                ]
+        }
     }
 
     return(
@@ -63,6 +91,15 @@ const ProductContainer = () => {
                 <View>
             <View>
                 <Banner />
+            </View>
+            <View>
+                <CategoryFilter 
+                    categories={categories}
+                    categoryFilter={changeCtg}
+                    productsCtg={productsCtg}
+                    active={active}
+                    setActive={setActive}
+                />
             </View>
             <View style= {{marginTop: 100}}>
             <FlatList
